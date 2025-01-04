55702b035cbc1cd57ffe6e6296d53c57d55d8eb0
sannanhaider <sannanhaider5@gmail.com>
2025-01-05 00:14:44 +0500

    Basic Routing Done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,71 +1,28 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-import { createPost, deletePost, getPosts } from "./api/api";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Landing from "./pages/landing";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import CreateBlog from "./pages/CreateBlog";
+import ReadBlog from "./pages/ReadBlog";
+import Profile from "./pages/Profile";
 
 function App() {
   // const base_url = process.env.API_BASE_URL || "http://localhost:8000";
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    const loadAllPosts = async () => {
-      try {
-        const res = await getPosts();
-        if (res.status === 200) {
-          console.log("Data fetched successfully: ", res.data);
-          setPosts(res.data);
-        }
-      } catch (error) {
-        console.error("Error fetching data: ", error);
-      }
-    };
-    loadAllPosts();
-  }, []);
-
-  const addPost = async () => {
-    try {
-      const newPost = {
-        title: "New Post",
-        author: "Monkie Uncle",
-        description: "This is a new post",
-        content: "This is a new post content",
-        created_at: new Date().toISOString(),
-      };
-
-      const res = await createPost(newPost);
-      if (res.status === 201) {
-        setPosts([...posts, res.data]);
-      }
-      console.log("Post added successFully");
-    } catch (error) {
-      console.error("Error adding post: ", error);
-    }
-  };
 
   return (
-    <>
-      <div>
-        <h1>Posts</h1>
-        {posts.length > 0 &&
-          posts.map((post) => (
-            <div key={post._id}>
-              <p>{post._id}</p>
-              <p>{post.title}</p>
-              <p>{post.author}</p>
-              <p>{post.description}</p>
-              <p>{post.content}</p>
-              <p>{post.created_at}</p>
-              <button
-                onClick={() => {
-                  deletePost(post._id);
-                }}
-              >
-                Delete
-              </button>
-            </div>
-          ))}
-      </div>
-      <button onClick={addPost}>Add Post</button>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/home " element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/create-blog" element={<CreateBlog />} />
+        <Route path="/read-blog/:id" element={<ReadBlog />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
   );
 }
 
