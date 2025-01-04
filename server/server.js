237f15b29248d208f4137e8ada0335a71d164741
sannanhaider <sannanhaider5@gmail.com>
2025-01-04 22:46:30 +0500

    Custom Middleware added

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import { connectToServer, getDb } from "./connect.js";
 import express from "express";
 import cors from "cors";
 import postRoutes from "./routes/postRoutes.js";
+import { logger } from "./middleware/posts.js";
 
 const port = process.env.PORT || 3001;
 
@@ -11,8 +12,16 @@ app.use(cors());
 // Parse incoming JSON
 app.use(express.json());
 
+// Custom Middleware
+app.use(logger);
+
 // Post Routes
 app.use("/api/posts", postRoutes);
+
+//Error Handling Middleware
+//Not found
+
+//Custom Error Handler
 
 app.listen(port, () => {
   connectToServer();
